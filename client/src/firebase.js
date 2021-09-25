e7535761a6aef11318f8e7960b9fb67c9e033312
malekaridhi <malek.aridhii@gmail.com>
2021-09-25 10:58:21 +0100

    first-commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import "firebase/auth"
 const firebaseConfig = {
-  apiKey: "AIzaSyBPwFwibSb-ZSRanJUq0M8XNOr4n3XCXaY",
-  authDomain: "mysog-f502c.firebaseapp.com",
-  projectId: "mysog-f502c",
-  storageBucket: "mysog-f502c.appspot.com",
-  messagingSenderId: "535892396626",
-  appId: "1:535892396626:web:30d08433abf8297b96ed8b"
+  apiKey: ,
+  authDomain: ,
+  projectId:,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
 const app= firebase.initializeApp(firebaseConfig);
 export const db = app.firestore();
